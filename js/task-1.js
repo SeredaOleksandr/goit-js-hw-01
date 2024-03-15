45e37e490bfdb9f5cee9a5baaf3273df86070a62
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-03-15 11:17:06 +0200

    Start

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+// const makeTransaction = (quantity, pricePerDroid);
+
+// const message = 'You ordered <quantity> droids worth <totalPrice> credits!';
+// console.log();
+
+// Перевірка;
+// const age = 16;
+
+// const isAdult = age >= 18;
+
+// console.log(isAdult);
+
+const value = '24.5px';
+
+console.log(isValid);
